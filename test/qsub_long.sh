25ad0bc965ca917ea5d67173abe5fa26187818a9
maximilian_hanussek <maximilian.hanussek@uni-tuebingen.de>
2020-04-15 11:23:09 +0200

    Changed test cases

--- test/qsub_long.sh
+++ test/qsub_long.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=1,walltime=55:00
+#PBS -l nodes=1:ppn=1,walltime=00:55:00
 
 /beeond/IDBA/idba_ud-1.0.9/bin/idba_ud -r /beeond/datasets/SRR741411.filt.fa --num_threads 1 -o output/
 sleep 1800
