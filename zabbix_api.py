a0522d88e688fbb91aeac5f9a1799b5ffc02e43c
maximilian_hanussek <maximilian.hanussek@uni-tuebingen.de>
2020-04-17 15:37:02 +0200

    Added healthcheck for TORQUE pbs_sched and automatated restart command

--- zabbix_api.py
+++ zabbix_api.py
@@ -65,6 +65,70 @@ result5 = zapi.do_request('trigger.update',
 	'status': 1
 })
 
+# Get host ID of Zabbix server
+result6 = zapi.do_request('host.get',
+{
+        'filter': {'host': 'Zabbix server'}
+})
+
+parsed_host_id_zabbix_server = result6['result'][0]['hostid']
+
+# Get hostinterface ID of Zabbix server
+result7 = zapi.do_request('hostinterface.get',
+{
+'output'	: 'extend',
+'hostids'	: parsed_host_id_zabbix_server
+})
+
+parsed_interface_id_zabbix_server = result7['result'][0]['interfaceid']
+
+# Create new healthcheck item for pbs_sched process
+result8 = zapi.do_request('item.create',
+{
+'name'		: 'Healthcheck for TORQUE pbs_sched process',
+'key_'		: 'proc.num[pbs_sched]',
+'hostid'	: parsed_host_id_zabbix_server,
+'type'		: 0,
+'value_type'	: 3,
+'interfaceid'	: parsed_interface_id_zabbix_server,
+'delay'	: '30s'
+})
+
+# Get id of newly created healthcheck item
+result9 = zapi.do_request('item.get',
+{
+'output'        : 'extend',
+'hostids'       : parsed_host_id_zabbix_server,
+'search'	: {'key_': 'proc.num'},
+'sortfield'	: 'name'
+})
+
+for i in range(len(result9['result'])):
+	if result9['result'][i]['name'] == "Healthcheck for TORQUE pbs_sched process":
+		array_entry = i
+
+parsed_item_id_healthcheck = result9['result'][array_entry]['itemid']
+
+# Create trigger for healthcheck
+result10 = zapi.do_request('trigger.create', 
+{
+'description'	: 'Healthcheck for TORQUE scheduler (pbs_sched) will be restarted auotmatically',
+'expression'	: '{Zabbix server:proc.num[pbs_sched].last()}=0',
+'priority'	: 4
+})
+
+# Create action for automatic scheduler restart
+result11 = zapi.do_request('action.create',
+{
+'name'          : 'Restart pbs_sched process',
+'esc_period'    : '2m',
+'eventsource'   : 0,
+'filter'        : {'evaltype': 0,
+		   'conditions': [{'conditiontype': 3, 'operator': 2, 'value': 'Healthcheck'}]},
+                   'operations': [{'operationtype': 1, 'opcommand_hst': [{'hostid': 0}], 'opcommand': {'type': 0, 'target_list': ['Current host'], 'execute_on': 1, 'command': 'sh /usr/local/bin/healthcheck_TORQUE_scheduler'}}
+                                 ]
+                  })
+
 
 # Logout from Zabbix
 zapi.user.logout()
