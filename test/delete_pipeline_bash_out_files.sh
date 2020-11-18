08cbbe3e436c15e0063fd00431e313d3c1228416
MaximilianHanussek <maximilian.hanussek@uni-tuebingen.de>
2020-11-18 16:20:01 +0100

    Create delete_pipeline_bash_out_files.sh

---
+++ test/delete_pipeline_bash_out_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf breakpoints*
+rm -rf cluster*
+rm -rf download_
+rm -rf download_*
+rm -rf dump_*
+rm -rf hap_align_*
+rm -rf hap_assemble_*
+rm -rf hap_cluster_*
+rm -rf merge_*
+rm -rf nhr_assemble.*
+rm -rf nhr_index.*
+rm -rf polish_*
+rm -rf sseq_align*
+rm -rf strand_assign.*
+rm -rf var_calling_*
+rm -rf wh_
+rm -rf wh_*
